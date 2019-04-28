4d2f554a47c48effd55cc2313752835da979cfd9
nikhil kapleshwari <nikhil1632@gmail.com>
2019-04-28 17:02:05 +0530

    added extra logging

--- MyApp/src/app/pwd-reset/pwd-reset.component.ts
+++ MyApp/src/app/pwd-reset/pwd-reset.component.ts
@@ -47,7 +47,9 @@ export class PwdResetComponent implements OnInit {
       dialog.init(()=>{
           setTimeout(()=>{
               this.authService.resetPassword(credentials).subscribe(response=>{
-                  console.log(JSON.stringify(response));
+                  console.log('response:'+JSON.stringify(response));
+                  console.log('response.json():'+response.json());
+                  console.log('message:'+response.json().message);
                   //dialog.find('.bootbox-body').html('<p>'+response.json().message+',Kindly login with new password</p>');
                   dialog.find('.bootbox-body').html('<p>Password reset,Kindly login with new password</p>');
                   this.router.navigate(['/login']);
